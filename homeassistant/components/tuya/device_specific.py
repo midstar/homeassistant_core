c6cf1413ae8f0c8da57fe3daeb40256eea0f1782
Joel Midstjärna <joel.midstjarna@gmail.com>
2022-12-03 15:54:31 +0100

    Added support for Royal Apex WiFi Thermostat

--- homeassistant/components/tuya/device_specific.py
+++ homeassistant/components/tuya/device_specific.py
@@ -6,6 +6,9 @@
 # https://developer.tuya.com/en/docs/iot/datatypedescription?id=K9i5ql2jo7j1k
 DEVICE_SPECIFIC_INT_BASE: dict[str, dict[str, int]] = {
     # BHT-002 Thermostat
-    "IAYz2WK1th0cMLmL": {"base_value": 2, "base_step": 10}
+    "IAYz2WK1th0cMLmL": {"base_value": 2, "base_step": 10},
+
+    # Royal Apex WiFi Thermostat
+    "N8bUqOZ8HBQjU0K0": {"base_value": 2, "base_step": 10}
     # Add more devices below
 }
